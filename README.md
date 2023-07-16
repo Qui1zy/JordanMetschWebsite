b84634b41821ef0a80e0b0fd90e72a2245aa5f54
Qui1zy <77511064+Qui1zy@users.noreply.github.com>
2023-07-16 09:39:47 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# JordanMetschWebsite
+# Portfolio
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Jordan-Metsch/pen/zYMpJrw](https://codepen.io/Jordan-Metsch/pen/zYMpJrw).
+
---
+++ src/script.js
@@ -0,0 +1,21 @@
+const para = document.createElement("p");
+const node = document.createTextNode("This is new.");
+para.appendChild(node);
+let count = 0;
+
+const element = document.getElementById("div1");
+const child = document.getElementById("p1");
+
+function roboticarm(){
+  count += 1;
+
+if (count == 1); {
+  element.insertBefore(para, child);
+}
+elif (count >= 2); {
+  element.
+}
+}
+
+
+
